7878e9ed19251b638a6b511c93b174d0a477cfe2
Hemant-14942 <hemantinsan6@gmail.com>
2025-05-31 17:24:32 +0530

    Update CORS origin to production URL

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(requestIp.mw());
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://url-shortlyy.vercel.app",
     credentials: true
   }));
   
