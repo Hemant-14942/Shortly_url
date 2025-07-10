fbed5cd531b9a9a77bf8ea57943c35fbeebacf8b
Hemant Sharma <141414237+Hemant-14942@users.noreply.github.com>
2025-07-10 14:49:06 +0530

    Update api.js
    added render backend deploy url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://shortlyurl-production.up.railway.app', // backend base URL
+  // baseURL: 'https://shortlyurl-production.up.railway.ap', // backend base URL
+  baseURL: 'https://shortly-url-2tmp.onrender.com', // backend base URL
   // baseURL: 'http://localhost:3000', // backend base URL
   withCredentials: true, // important: allows cookies (for sessions or auth)
   headers: {
